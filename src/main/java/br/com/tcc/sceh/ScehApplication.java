3701cb6c23b50f07ed63b410544828f4f7b4d9a2
Luccas Santos <luccas.santos@ab-inbev.com>
2021-09-22 21:41:32 -0300

    Criando estrutura basica do projeto SCEH

---
+++ src/main/java/br/com/tcc/sceh/ScehApplication.java
@@ -0,0 +1,13 @@
+package br.com.tcc.sceh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScehApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScehApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/tcc/sceh/ScehApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.tcc.sceh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScehApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
